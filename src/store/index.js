696974c8ae19007f5b3bb2c219d3db4b8d3474d9
Rafael Atheya <rafael.satriabhi@gmail.com>
2020-09-18 01:30:26 +0700

    there's hope

--- src/store/index.js
+++ src/store/index.js
@@ -5,13 +5,24 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    words: [
-      'Architecture', 'Programming', 'Asyawaladu', 'Swaladubab', 'Cangcimen', 'Borokokok'
-    ]
+    words: [],
+    count: 0
   },
   mutations: {
+    loadingWords (state, payload) {
+      state.words = payload
+    },
+    incrementCount (state) {
+      state.count++
+    }
   },
   actions: {
+    loadingWords (context, payload) {
+      context.commit('loadingWords', payload)
+    },
+    incrementCount (context) {
+      context.commit('incrementCount')
+    }
   },
   modules: {
   }
