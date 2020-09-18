04e61ce230148b754daa92a2d88e5720abb9869a
ifantrifandi <ifan.trifandi@gmail.com>
2020-09-18 10:37:10 +0700

    testing progress bar

--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -3,22 +3,22 @@
     <div
       class="progress-bar progress-bar-striped bg-warning"
       role="progressbar"
-      style="width: 0%"
+      v-bind:style="`width: ${user.score}%`"
       aria-valuenow="75"
       aria-valuemin="0"
       aria-valuemax="100"
     >
-      <div class="mt-2 text-dark">{{name}}</div>
+      <div class="mt-2 text-dark">{{user.name}}</div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "ProgressBar",
-  props: ["score", "name"],
-  methods: {},
-};
+  name: 'ProgressBar',
+  props: ['user'],
+  methods: {}
+}
 </script>
 
 <style>
